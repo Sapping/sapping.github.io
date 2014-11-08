5cf48f91de6e02abbce76fabba223c88e670f0db
Sapping <lucas.melin@gmail.com>
2014-11-08 12:36:26 -0500

    Deploy.

---
+++ _harp/_deploy.sh
@@ -0,0 +1,17 @@
+# change dirs to _harp if not already in there
+if [ ${PWD##*/} != "_harp" ]; then
+  cd _harp
+fi
+
+# source is the current directory and the target the parent directory
+harp compile ./ ../
+
+# files starting with _ are not copied to target. copy _README and rename
+cp _README.md ../
+cd ..
+mv _README.md README.md
+
+# commit to the repo and push to github
+git add .
+git commit -m "Deploy."
+git push
